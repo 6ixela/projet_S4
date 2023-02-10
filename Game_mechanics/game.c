900a5691c7681db0c01e93e67b38e0d607a5de90
Luanico <lucasmu07@gmail.com>
2023-02-10 19:48:57 +0100

    send test
    Signed-off-by: Luanico <lucasmu07@gmail.com>

--- Game_mechanics/game.c
+++ Game_mechanics/game.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include "game.h"
 
+
 struct piece *newPiece(char *name)
 {
     struct piece *res =  malloc(sizeof(struct piece));
@@ -25,8 +26,62 @@ struct piece *newPiece(char *name)
     if(res)
     {
         res->hasMoved = 0;
+        res->isWhite = 0;
         res->name = malloc(len);
         memcpy(res->name, name, len+1);
+    }
+    return res;
+}
+
+struct piece **newBoard()
+{
+    //malloc board size: 64 pointers
+    struct piece **board = malloc(sizeof(struct piece*) * 64);
+    for (int i = 0; i<64; i++)
+    {
+        board[i] = NULL;
+    }
+
+    //list to place pieces
+    char** piecesNames = {"rook", "knight", "bishop", "queen", "king",
+    "bishop", "knight", "rook"};
+
+    for(int x = 0; x<8; x++)
+    {
+        int posW1 = 8+x;
+        int posW2 = x;
+        int posB1 = 48+x;
+        int posB2 = 56+x;
+        //place pawns
+        placePiece(board, "pawn", posW1);
+        placePiece(board, "pawn", posB1);
+        board[posW1]->isWhite = 1;
+
+        //place others
+        placePiece(board, piecesNames[x], posW2);
+        placePiece(board, piecesNames[x], posB2);
+        board[posW2]->isWhite = 1;
+    }
+
+    return board;
+}
+
+int placePiece(struct piece **board, char* name, int pos)
+{
+    int res = 0;
+    if(pos < 0 || pos > 63)
+    {
+        printf("place_piece: Error in position\n");
+        res = -1;
+    }
+    else if(board[pos] != NULL)
+    {
+        printf("place_piece: A piece is already there!\n");
+        res = 1;
+    }
+    else
+    {
+        board[pos] = new_piece(name);
     }
     return res;
 }
--- Game_mechanics/game.h
+++ Game_mechanics/game.h
@@ -4,6 +4,7 @@
 struct piece 
 {
     char *name;
+    int isWhite;
     int hasMoved;
     int value;
 };
@@ -11,5 +12,14 @@ struct piece
 //initializes a new piece with the given name
 //returns null if wrong name
 struct piece *newPiece (char* name);
+
+//Creates a new board and places every base piece on it
+//returns the board(a list of pointers of pieces)
+struct piece **newBoard();
+
+//Places a piece at a given place
+//board: the board to place; name: the piece name; pos: the position to place
+//returns 0 if worked, -1 if position problem, 1 if there is already a piece
+int placePiece(struct piece **board, char* name, int pos);
 
 #endif
